7f9c75ab97c662d43646a95b4043493f28da98cf
andy <andy@example.com>
2022-02-19 14:45:38 +0000

    fagent

--- src/ebay_api.rs
+++ src/ebay_api.rs
@@ -25,19 +25,19 @@ impl Tokens {
 
 #[derive(Debug, Clone, serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct EbayApi {
-  pub shop_name: String,
-  pub tokens: Tokens,
-  pub auth_url: AuthUrl,
-  pub token_url: TokenUrl,
-  pub client_id: ClientId,
-  pub client_secret: ClientSecret,
-  pub scope: Scope,
-  pub url_sceme: String,
-  pub limit: String,
+    pub shop_name: String,
+    pub tokens: Tokens,
+    pub auth_url: AuthUrl,
+    pub token_url: TokenUrl,
+    pub client_id: ClientId,
+    pub client_secret: ClientSecret,
+    pub scope: Scope,
+    pub url_sceme: String,
+    pub limit: String,
 }
 
 impl EbayApi {
-    pub async fn new(shop_name:&str) -> Result<Self, Box<dyn std::error::Error>> {
+    pub async fn new(shop_name: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let key = ["oauth_token_ebay_", shop_name].concat();
         let tokens = if let Ok(Some(x)) = DB.get(&key) {
             serde_json::from_str(std::str::from_utf8(&x).unwrap()).unwrap()
@@ -63,235 +63,250 @@ impl EbayApi {
 
     pub async fn get(&mut self, api_endpoint: &str) -> Result<String, Box<dyn std::error::Error>> {
         // переделать в метод структуры токенс
-       let mut reply = String::new();
-       for i in 1..=3 {        // перенести эту проверку внутрь get
-           let mut params: HashMap<&str, String> = HashMap::new();
-           params.insert( "limit",self.limit.clone());
-           let url = [API_URL, api_endpoint].concat();
-           let client = reqwest::Client::new();
-           reply = client
-               .get(url)
-               .query(&params)
-               .header("Authorization", ["Bearer ", &self.tokens.access_token].concat())
-               .header("Content-Type", "application/json")
-               .send()
-               .await?
-               .text()
-               .await?;
-           if !reply.contains("errorId") {
-               break
-           } else {
-               if reply.contains("Invalid access token") {
-                   println!("Invalid access token")
-               } else {
-                   println!("Reply body: {}", reply);
-               }
-               match i {
-                   1 => self.refresh_access_token(true).await?,
-                   2 => self.auth().await?,
-                   _ => println!("Error during token exchagne cycle"),
-               }
-           }
-       }
-       Ok(reply)
-   }
-
-   pub async fn post(&mut self, api_endpoint: &str, call_name: &str, body: String) -> Result<String, Box<dyn std::error::Error>> {
-
-   let mut reply = String::new();
-   for i in 1..=3 {
-
-       let url = [API_URL, api_endpoint].concat();
-       let client = reqwest::Client::new();
-       reply = client
-           .post(url)
-           .body(body.clone())
-           .header("Content-Type", "text/xml")
-           .header("X-EBAY-API-SITEID", "3")
-           .header("X-EBAY-API-COMPATIBILITY-LEVEL", "1225")
-           .header("X-EBAY-API-IAF-TOKEN", &self.tokens.access_token)
-           .header("X-EBAY-API-CALL-NAME", call_name)
-           .send()
-           .await?
-           .text()
-           .await?;
-       if !reply.contains("rrors") {
-           break
-       } else {
-
-           let a = "Invalid access token";
-           let b = "IAF token supplied is expired";
-           let mut bad_token = true;
-
-           if reply.contains(a)         { println!("{} - {}", self.shop_name, a) }
-           else if reply.contains(b)    { println!("{} - {}", self.shop_name, b) }
-           else { 
-                println!("{} - request has failed: {}", self.shop_name, reply);
-                bad_token = false;           
-            }
-            if bad_token {
+        let mut reply = String::new();
+        for i in 1..=3 {
+            // перенести эту проверку внутрь get
+            let mut params: HashMap<&str, String> = HashMap::new();
+            params.insert("limit", self.limit.clone());
+            let url = [EBAY_API_URL, api_endpoint].concat();
+            let client = reqwest::Client::new();
+            reply = client
+                .get(url)
+                .query(&params)
+                .header(
+                    "Authorization",
+                    ["Bearer ", &self.tokens.access_token].concat(),
+                )
+                .header("Content-Type", "application/json")
+                .send()
+                .await?
+                .text()
+                .await?;
+            if !reply.contains("errorId") {
+                break;
+            } else {
+                if reply.contains("Invalid access token") {
+                    println!("Invalid access token")
+                } else {
+                    println!("ebay_api.get has returned error: {}", reply);
+                }
+                if reply.contains("System error") {
+                    break;
+                }
                 match i {
                     1 => self.refresh_access_token(true).await?,
                     2 => self.auth().await?,
                     _ => println!("Error during token exchagne cycle"),
                 }
             }
-       }
-   }
-   Ok(reply)
-}
+        }
+        Ok(reply)
+    }
 
-pub async fn auth(&mut self) -> Result<(), Box<dyn std::error::Error>> {       
+    pub async fn post(
+        &mut self,
+        api_endpoint: &str,
+        call_name: &str,
+        body: String,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut reply = String::new();
+        for i in 1..=3 {
+            let url = [EBAY_API_URL, api_endpoint].concat();
+            let client = reqwest::Client::new();
+            reply = client
+                .post(url)
+                .body(body.clone())
+                .header("Content-Type", "text/xml")
+                .header("X-EBAY-API-SITEID", "3")
+                .header("X-EBAY-API-COMPATIBILITY-LEVEL", "1225")
+                .header("X-EBAY-API-IAF-TOKEN", &self.tokens.access_token)
+                .header("X-EBAY-API-CALL-NAME", call_name)
+                .send()
+                .await?
+                .text()
+                .await?;
+            if !reply.contains("rrors") {
+                break;
+            } else {
+                let a = "Invalid access token";
+                let b = "IAF token supplied is expired";
+                let mut bad_token = true;
 
-    // Set up the config for the Github OAuth2 process.
-    let client = BasicClient::new(
-        self.client_id.clone(),
-        Some(self.client_secret.clone()),
-        self.auth_url.clone(),
-        Some(self.token_url.clone()),
-    );
+                if reply.contains(a) {
+                    println!("{} - {}", self.shop_name, a)
+                } else if reply.contains(b) {
+                    println!("{} - {}", self.shop_name, b)
+                } else {
+                    println!("{} - request has failed: {}", self.shop_name, reply);
+                    bad_token = false;
+                }
+                if bad_token {
+                    match i {
+                        1 => self.refresh_access_token(true).await?,
+                        2 => self.auth().await?,
+                        _ => println!("Error during token exchagne cycle"),
+                    }
+                }
+            }
+        }
+        Ok(reply)
+    }
 
-    // Generate the authorization URL to which we'll redirect the user.
-    let (authorize_url, csrf_state) = client
-        .authorize_url(|| CsrfToken::new("http://localhost:8080".to_string()))
-        .add_scope(self.scope.clone())
-        .add_extra_param("redirect_uri", &self.url_sceme)
-        .url();
+    pub async fn auth(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        // Set up the config for the Github OAuth2 process.
+        let client = BasicClient::new(
+            self.client_id.clone(),
+            Some(self.client_secret.clone()),
+            self.auth_url.clone(),
+            Some(self.token_url.clone()),
+        );
 
-    // let url = format!("start {}", authorize_url.to_string()).replace("&", "^&");
+        // Generate the authorization URL to which we'll redirect the user.
+        let (authorize_url, csrf_state) = client
+            .authorize_url(|| CsrfToken::new("http://localhost:8080".to_string()))
+            .add_scope(self.scope.clone())
+            .add_extra_param("redirect_uri", &self.url_sceme)
+            .url();
 
-    // let output = Command::new("cmd.exe").args(["/C", &url]).output();
+        // let url = format!("start {}", authorize_url.to_string()).replace("&", "^&");
 
-    // if let Err(e) = output {
+        // let output = Command::new("cmd.exe").args(["/C", &url]).output();
+
+        // if let Err(e) = output {
         // println!("We couldn't start your browser: {:?}\n\n
         // Please start it manually and open this URL:\n{}\n", e, authorize_url.to_string());
-    // }
+        // }
 
-    println!("Please open this URL:\n{}\n", authorize_url.to_string());
+        println!("Please open this URL:\n{}\n", authorize_url.to_string());
 
-    // A very naive implementation of the redirect server.
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    loop {
-        if let Ok((mut stream, _)) = listener.accept().await {
-            let code;
-            let state;
-            {
-                let mut reader = BufReader::new(&mut stream);
+        // A very naive implementation of the redirect server.
+        let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+        loop {
+            if let Ok((mut stream, _)) = listener.accept().await {
+                let code;
+                let state;
+                {
+                    let mut reader = BufReader::new(&mut stream);
 
-                let mut request_line = String::new();
-                reader.read_line(&mut request_line).await.unwrap();
+                    let mut request_line = String::new();
+                    reader.read_line(&mut request_line).await.unwrap();
 
-                let redirect_url = request_line.split_whitespace().nth(1).unwrap();
-                let url = Url::parse(&("http://localhost".to_string() + redirect_url)).unwrap();
+                    let redirect_url = request_line.split_whitespace().nth(1).unwrap();
+                    let url = Url::parse(&("http://localhost".to_string() + redirect_url)).unwrap();
 
-                let code_pair = url
-                    .query_pairs()
-                    .find(|pair| {
-                        let &(ref key, _) = pair;
-                        key == "code"
-                    })
-                    .unwrap();
+                    let code_pair = url
+                        .query_pairs()
+                        .find(|pair| {
+                            let &(ref key, _) = pair;
+                            key == "code"
+                        })
+                        .unwrap();
 
-                let (_, value) = code_pair;
-                code = AuthorizationCode::new(value.into_owned());
+                    let (_, value) = code_pair;
+                    code = AuthorizationCode::new(value.into_owned());
 
-                let state_pair = url
-                    .query_pairs()
-                    .find(|pair| {
-                        let &(ref key, _) = pair;
-                        key == "state"
-                    })
-                    .unwrap();
+                    let state_pair = url
+                        .query_pairs()
+                        .find(|pair| {
+                            let &(ref key, _) = pair;
+                            key == "state"
+                        })
+                        .unwrap();
 
-                let (_, value) = state_pair;
-                state = CsrfToken::new(value.into_owned());
-            }
+                    let (_, value) = state_pair;
+                    state = CsrfToken::new(value.into_owned());
+                }
 
-            let message = "Go back to your terminal :)";
-            let response = format!(
-                "HTTP/1.1 200 OK\r\ncontent-length: {}\r\n\r\n{}",
-                message.len(),
-                message
-            );
-            stream.write_all(response.as_bytes()).await.unwrap();
-
-            println!("Request returned the following code:\n{}\n", code.secret());
-            println!(
-                "Request returned the following state:\n{} (expected `{}`)\n",
-                state.secret(),
-                csrf_state.secret()
-            );
-
-            // Exchange the code with a token.
-            let token_res = client
-                .exchange_code(code)
-                .add_extra_param("redirect_uri", &self.url_sceme)
-                .request_async(async_http_client)
-                .await;
-
-            println!("Github returned the following token:\n{:?}\n", token_res);
-
-            if let Ok(token) = token_res {
-                // обрабатывать если вернул ошибку!!!
-                let tokens = Tokens::new(
-                    token.access_token().secret().clone(),
-                    token.expires_in().unwrap().as_secs(),
-                    token.refresh_token().unwrap().secret().clone(),
+                let message = "Go back to your terminal :)";
+                let response = format!(
+                    "HTTP/1.1 200 OK\r\ncontent-length: {}\r\n\r\n{}",
+                    message.len(),
+                    message
                 );
-                dbg!(&tokens);
+                stream.write_all(response.as_bytes()).await.unwrap();
 
-                let key = ["oauth_token_ebay_", &self.shop_name].concat();
-                DB.insert(&key, serde_json::to_string(&tokens).unwrap().as_bytes())
-                    .unwrap();
-                self.tokens.access_token = token.access_token().secret().clone();
+                println!("Request returned the following code:\n{}\n", code.secret());
+                println!(
+                    "Request returned the following state:\n{} (expected `{}`)\n",
+                    state.secret(),
+                    csrf_state.secret()
+                );
 
+                // Exchange the code with a token.
+                let token_res = client
+                    .exchange_code(code)
+                    .add_extra_param("redirect_uri", &self.url_sceme)
+                    .request_async(async_http_client)
+                    .await;
+
+                println!("Github returned the following token:\n{:?}\n", token_res);
+
+                if let Ok(token) = token_res {
+                    // обрабатывать если вернул ошибку!!!
+                    let tokens = Tokens::new(
+                        token.access_token().secret().clone(),
+                        token.expires_in().unwrap().as_secs(),
+                        token.refresh_token().unwrap().secret().clone(),
+                    );
+                    dbg!(&tokens);
+
+                    let key = ["oauth_token_ebay_", &self.shop_name].concat();
+                    DB.insert(&key, serde_json::to_string(&tokens).unwrap().as_bytes())
+                        .unwrap();
+                    self.tokens.access_token = token.access_token().secret().clone();
+                }
+                break;
             }
-            break;
         }
+        Ok(())
     }
-    Ok(())
-}
 
-pub async fn refresh_access_token(&mut self, print: bool) -> Result<(), Box<dyn std::error::Error>> {
-    if print { print!("Refreshing access token... ") };
-  let client = BasicClient::new(
-      self.client_id.clone(), 
-Some(self.client_secret.clone()),
-self.auth_url.clone(),
-Some(self.token_url.clone()),
-  );
+    pub async fn refresh_access_token(
+        &mut self,
+        print: bool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if print {
+            print!("Refreshing access token... ")
+        };
+        let client = BasicClient::new(
+            self.client_id.clone(),
+            Some(self.client_secret.clone()),
+            self.auth_url.clone(),
+            Some(self.token_url.clone()),
+        );
 
-  let res = client
-      .exchange_refresh_token(&RefreshToken::new(self.tokens.refresh_token.clone()))
-      .add_scope(self.scope.clone())
-      .request_async(async_http_client)
-      .await;
+        let res = client
+            .exchange_refresh_token(&RefreshToken::new(self.tokens.refresh_token.clone()))
+            .add_scope(self.scope.clone())
+            .request_async(async_http_client)
+            .await;
 
-  // dbg!(&res);
+        // dbg!(&res);
 
-  // if let Ok(token) = res {
-  // обрабатывать если вернул ошибку!!!
-  let token = match res {
-      Ok(x) => x,
-      Err(e) => {
-          println!("Token refresh has failed with following error: {}", e);
-          return Ok(());
-      },
-  };
+        // if let Ok(token) = res {
+        // обрабатывать если вернул ошибку!!!
+        let token = match res {
+            Ok(x) => x,
+            Err(e) => {
+                println!("Token refresh has failed with following error: {}", e);
+                return Ok(());
+            }
+        };
 
-  let tokens = Tokens::new(
-      token.access_token().secret().clone(),
-      token.expires_in().unwrap().as_secs(),
-      self.tokens.refresh_token.to_string(),
-  );
+        let tokens = Tokens::new(
+            token.access_token().secret().clone(),
+            token.expires_in().unwrap().as_secs(),
+            self.tokens.refresh_token.to_string(),
+        );
 
-  let key = ["oauth_token_ebay_", &self.shop_name].concat();
-  DB.insert(&key, serde_json::to_string(&tokens).unwrap().as_bytes())
-      .unwrap();
-  
-  self.tokens.access_token = token.access_token().secret().clone();
-  if print { println!("OK") };
-  Ok(())
-}
-  
+        let key = ["oauth_token_ebay_", &self.shop_name].concat();
+        DB.insert(&key, serde_json::to_string(&tokens).unwrap().as_bytes())
+            .unwrap();
+
+        self.tokens.access_token = token.access_token().secret().clone();
+        if print {
+            println!("OK")
+        };
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
-mod models;
 mod ebay_api;
-// mod fagent_api;
+mod fagent_api;
 mod feedback;
-use models::*;
+mod models;
 use ebay_api::*;
+use fagent_api::*;
+use models::*;
 
 use reqwest;
 use std::collections::HashMap;
 // use std::process::Command;
+use chrono::prelude::*;
+use config::Config;
 use oauth2::basic::BasicClient;
 use oauth2::reqwest::async_http_client;
 use oauth2::{
-    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken,
-    RefreshToken, Scope, TokenResponse, TokenUrl,
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, RefreshToken, Scope,
+    TokenResponse, TokenUrl,
 };
+use once_cell::sync::Lazy;
+use sled::Db;
+use std::collections::HashSet;
+use std::time::Duration;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use url::Url;
-use once_cell::sync::Lazy;
-use config::Config;
-use sled::Db;
-use chrono::prelude::*;
 use tokio::time;
-use std::time::Duration;
-use std::collections::HashSet;
+use url::Url;
 // use serde_derive::{Serialize, Deserialize};
 
 static CONF: Lazy<Config> = Lazy::new(|| {
@@ -36,37 +37,124 @@ static CONF: Lazy<Config> = Lazy::new(|| {
 
 static DB: Lazy<Db> = Lazy::new(|| sled::open("db").expect("Can't open the DB"));
 
-pub const API_URL: &str = "https://api.ebay.com";
+pub const EBAY_API_URL: &str = "https://api.ebay.com";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let debug = CONF.get::<bool>("debug").unwrap();
+    if debug {
+        // println!("Running in DEBUG mode");
+
+        let mut fagent_api = FagentApi::new().await?;
+
+        // let reply = fagent_api.get("categories").await?;
+
+        let api_endpoint = format!("bank_transaction_explanations");
+
+        let date = Utc::today().format("%Y-%m-%d");
+
+        struct BankTransaction {
+            account: i32,
+            description: &'static str,
+            category: &'static str,
+            gross_value: i32,
+        }
+
+        let transactions = [
+            BankTransaction {
+                account: 1030038,
+                description: "Amazon sales",
+                category: "002",
+                gross_value: 100,
+            },
+            BankTransaction {
+                account: 1030038,
+                description: "Amazon fees",
+                category: "160",
+                gross_value: -33,
+            },
+            BankTransaction {
+                account: 1030344,
+                description: "Ebay sales Mobriver",
+                category: "003",
+                gross_value: 100,
+            },
+            BankTransaction {
+                account: 1030344,
+                description: "Ebay fees",
+                category: "161",
+                gross_value: -33,
+            },
+            BankTransaction {
+                account: 1030351,
+                description: "Ebay sales Spasimira",
+                category: "004",
+                gross_value: 100,
+            },
+            BankTransaction {
+                account: 1030351,
+                description: "Ebay fees",
+                category: "161",
+                gross_value: -33,
+            },
+        ];
+
+        for t in transactions {
+            let body = format!(
+                r#"
+                {{ "bank_transaction_explanation":
+                    {{
+                        "bank_account":"https://api.freeagent.com/v2/bank_accounts/{}",
+                        "dated_on":"{date}",
+                        "description":"{}",
+                        "category":"https://api.freeagent.com/v2/categories/{}",
+                        "gross_value":"{}"
+                    }}
+                }}
+            "#,
+                t.account, t.description, t.category, t.gross_value
+            );
+            // let reply = fagent_api.post(&api_endpoint, body).await?;
+            // println!("{reply}");
+        }
+
+        // return Ok(());
+
+        // let reply = fagent_api.post(&api_endpoint, body).await?;
+
+        // println!("{reply}");
+
+        // println!("Exiting... OK");
+        return Ok(());
+    }
+
     let write_orders_and_send_messages = CONF.get::<bool>("send_messages").unwrap();
-    let mut interval_5_min = time::interval(Duration::from_secs(5*60));
+    let mut interval_5_min = time::interval(Duration::from_secs(5 * 60));
     let shops = CONF.get::<HashSet<String>>("ebay.shops").unwrap();
     let shops_for_feedback = CONF.get::<HashSet<String>>("shops_for_feedback").unwrap();
     let shops_for_refresh: HashSet<&String> = shops_for_feedback.union(&shops).collect();
 
-        //     // spawn tasks that run in parallel
-        // let tasks: Vec<_> = shops_for_refresh
-        //     .iter()
-        //     .map(|mut shop_name| {
-        //         tokio::spawn(async {
-        //             let mut web = Web::new(shop_name).await.unwrap();
-        //             web.refresh(false)
-        //         })
-        //     })
-        //     .collect();
-        // // now await them to get the resolve's to complete
-        // for task in tasks {
-        //     task.await.unwrap();
-        // }
+    //     // spawn tasks that run in parallel
+    // let tasks: Vec<_> = shops_for_refresh
+    //     .iter()
+    //     .map(|mut shop_name| {
+    //         tokio::spawn(async {
+    //             let mut web = Web::new(shop_name).await.unwrap();
+    //             web.refresh(false)
+    //         })
+    //     })
+    //     .collect();
+    // // now await them to get the resolve's to complete
+    // for task in tasks {
+    //     task.await.unwrap();
+    // }
 
     let mut i = 0;
     let mut print_timer = 0;
     loop {
         interval_5_min.tick().await;
 
-        if i == 0 { 
+        if i == 0 {
             feedback::leave().await?;
             for shop_name in &shops_for_refresh {
                 let mut ebay_api = EbayApi::new(shop_name).await?;
@@ -74,8 +162,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
         i += 1;
-        if i == 20 { i = 0 }
-        
+        if i == 20 {
+            i = 0
+        }
+
         print_timer += 1;
         if print_timer == 100 {
             println!("+");
@@ -83,7 +173,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         for shop_name in &shops {
-
             let mut ebay_api = EbayApi::new(shop_name).await?;
 
             let mut orders: HashSet<String> = if let Ok(Some(x)) = DB.get("orders") {
@@ -100,14 +189,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let deserializer = &mut serde_json::Deserializer::from_str(&reply);
             let json: EbayOrders = match serde_path_to_error::deserialize(deserializer) {
                 Ok(json) => json,
-                Err(e) =>  {
-                        println!("Deserealisation error: {}\nReply body: ._{}_.", e, reply);
-                        continue;
-                    },
+                Err(e) => {
+                    println!("Deserealisation error: {}\nReply body: ._{}_.", e, reply);
+                    continue;
+                }
             };
 
             for order in json.orders {
-                let total: f64 = order.pricing_summary.total.value.clone().parse().unwrap_or_default();
+                let total: f64 = order
+                    .pricing_summary
+                    .total
+                    .value
+                    .clone()
+                    .parse()
+                    .unwrap_or_default();
                 if total > CONF.get::<f64>("ebay.sale_to_notify").unwrap() {
                     let order_id = order.order_id;
 
@@ -118,22 +213,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                         if write_orders_and_send_messages {
                             let bot_url = "https://api.telegram.org/bot863650897:AAE-usx-Av7yk0C1csClrS-nFLgDzVTrNmo/sendMessage?chat_id=-1001451097938&text=";
-                            let url = format!("{}£{} from {} for {}", bot_url, total, shop_name, order.line_items[0].title);
-                            reqwest::get(url)
-                                .await?
-                                .text()
-                                .await?;
+                            let url = format!(
+                                "{}£{} from {} for {}",
+                                bot_url, total, shop_name, order.line_items[0].title
+                            );
+                            reqwest::get(url).await?.text().await?;
                         }
                     }
                 }
             }
             if new_orders_found && write_orders_and_send_messages {
-                DB.insert("orders", serde_json::to_string(&orders).unwrap().as_bytes()).unwrap();
+                DB.insert("orders", serde_json::to_string(&orders).unwrap().as_bytes())
+                    .unwrap();
             }
         }
         DB.flush()?;
     }
     // Ok(())
 }
-
-
