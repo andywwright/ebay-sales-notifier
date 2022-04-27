d7a6d98323634173cc24a656c8a8054a21c203d1
andy <andy@example.com>
2022-04-27 10:36:01 +0100

    save

--- src/feedback.rs
+++ src/feedback.rs
@@ -93,6 +93,7 @@ pub async fn leave() -> Result<(), Box<dyn std::error::Error>> {
             .collect();
 
         if positive.is_empty() {
+            println!("{shop_name} - No awaiting feedback found as positive is empty");
             continue;
         }
         for awaiting_feedback in positive {
